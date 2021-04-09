63f0d6cda6c293257a996de187959fea9b8fc104
usausa <machi.pon@gmail.com>
2021-04-09 19:05:06 +0900

    Update

---
+++ AspNetCoreComponents.IpFilter/PathRestrictConfig.cs
@@ -0,0 +1,15 @@
+namespace AspNetCoreComponents.IpFilter
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Net;
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1819:PropertiesShouldNotReturnArrays", Justification = "Ignore")]
+    internal class PathRestrictConfig
+    {
+        [AllowNull]
+        public string Path { get; set; }
+
+        [AllowNull]
+        public IPNetwork[] Networks { get; set; }
+    }
+}
---
+++ AspNetCoreComponents.IpFilter/PathRestrictMiddleware.cs
@@ -0,0 +1,52 @@
+namespace AspNetCoreComponents.IpFilter
+{
+    using System;
+    using System.Net;
+    using System.Runtime.CompilerServices;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Http;
+
+    internal class PathRestrictMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        private readonly string path;
+
+        private readonly IPNetwork[] networks;
+
+        public PathRestrictMiddleware(RequestDelegate next, PathRestrictConfig config)
+        {
+            this.next = next;
+            path = config.Path;
+            networks = config.Networks;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Path.StartsWithSegments(path, StringComparison.OrdinalIgnoreCase) &&
+                ((context.Request.HttpContext.Connection.RemoteIpAddress is null) ||
+                 !IsAddressAllowed(context.Request.HttpContext.Connection.RemoteIpAddress)))
+            {
+                context.Response.StatusCode = 403;
+                return;
+            }
+
+            await next(context).ConfigureAwait(false);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool IsAddressAllowed(IPAddress address)
+        {
+            for (var i = 0; i < networks.Length; i++)
+            {
+                if (networks[i].Contains(address))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
