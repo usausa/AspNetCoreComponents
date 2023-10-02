4b71a19ccdd57a20cf9cf47d04e56dc63c9b7b98
usausa <machi.pon@gmail.com>
2023-10-02 12:32:13 +0900

    Update setting

--- Example.Web/GlobalSuppressions.cs
+++ Example.Web/GlobalSuppressions.cs
@@ -1,1 +1,2 @@
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:DoNotDirectlyAwaitATaskAnalyzer", Justification = "Ignore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1848:Use the LoggerMessage delegates", Justification = "Ignore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Do not directly await a Task", Justification = "Ignore")]
